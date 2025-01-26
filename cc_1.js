a576872b6ea5d68c440ec8e7367ba3d2103cc44b
Maidele Archelus <maidele@usf.edu>
2025-01-25 21:22:58 -0500

    removed task 1 variables

--- cc_1.js
+++ cc_1.js
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JavaScript Basics Challenge</title>
-</head>
-<body>
-    <h1>JavaScript Basics Coding Challenge</h1>
-    <script src="cc_1.js"></script>
-</body>
-</html>
 
 //Task 1: Variables
 let employeeName = "Jessica Parks"; //String - Employee's name 

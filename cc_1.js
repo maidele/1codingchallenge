657902b055b5c0e17cea66749e0ca771a1d4de2f
Maidele Archelus <maidele@usf.edu>
2025-01-25 23:01:28 -0500

    Task 2 - Product Details.

--- cc_1.js
+++ cc_1.js
@@ -3,7 +3,16 @@
 let employeeName = "Jessica Parks"; //String - Employee's name 
 const employeeID = 7777; // Number - ID for the Employee
 var isActive = true; // Boolean value- Employee is isActive
-console.log(typeof employeeName)
-console.log(typeof employeeID) 
-console.log(type of isActive) 
+console.log(typeof employeeName);
+console.log(typeof employeeID); 
+console.log(typeof isActive) ;
+
+//Task 2: Primitive Data Types
+let productName = "Iphone 13"; // name of Iphone
+const productPrice = 800; //cost of Iphone
+var isAvaiable = true; // Iphone is available
+console.log(typeof productName);
+console.log(typeof productPrice);
+console.log(typeof isAvaiable);
+
 
